fc98111179ce160e6352a8c63c1670087c972c76
Pramila-star <aiml23028@glbitm.ac.in>
2026-07-07 21:45:15 +0530

    Buy Max Sell Profit solution

---
+++ MATHS/BuyMaxSellProfit.java
@@ -0,0 +1,27 @@
+package MATHS;
+
+public class BuyMaxSellProfit {
+     public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+    }
+}
