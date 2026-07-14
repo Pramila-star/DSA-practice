a49801f3e6b486740c51292c44b3756acba22dd9
Pramila-star <aiml23028@glbitm.ac.in>
2026-07-14 23:28:55 +0530

    Word search

---
+++ MATHS/WordSearch.java
@@ -0,0 +1,68 @@
+package MATHS;
+
+import java.util.Scanner;
+
+public class WordSearch {
+        public static boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean dfs(char[][] board, String word, int row, int col, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
+            return false;
+        }
+
+        if (board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        boolean found = dfs(board, word, row + 1, col, index + 1)
+                || dfs(board, word, row - 1, col, index + 1)
+                || dfs(board, word, row, col + 1, index + 1)
+                || dfs(board, word, row, col - 1, index + 1);
+
+        board[row][col] = temp;
+
+        return found;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        char[][] board = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        String word = sc.next();
+
+        System.out.println(exist(board, word));
+
+        sc.close();
+    }
+
+}
