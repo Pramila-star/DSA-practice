041fd04040b37b7c1d68fe9f1adf02ae83c11ac1
Pramila-star <aiml23028@glbitm.ac.in>
2026-07-15 22:07:47 +0530

    GCD of ODD and EVEN sums

---
+++ MATHS/GCDofOddAndEvenSum.java
@@ -0,0 +1,12 @@
+package MATHS;
+
+public class GCDofOddAndEvenSum {
+    public static int  gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+    public static void main(String args[]){
+        int n = 4;
+        System.out.println( gcdOfOddEvenSums(n));
+    }
+}
